9a2f663e303b30b24172e45f861433498d952068
Yura <gyrra777@gmail.com>
2023-06-20 20:06:17 +0300

    Страницу логина в админку /admin

--- tests/Admin_login_page.py
+++ tests/Admin_login_page.py
@@ -1,1 +1,31 @@
 from selenium.webdriver.common.by import By
+
+
+def test_catalog_accessible_name(browser):
+    browser.get('http://192.168.31.208:8081/admin/')
+    username_form = browser.find_element(By.CSS_SELECTOR, "#input-username")
+    assert username_form.accessible_name == "Username"
+
+
+def test_catalog_accessible_password(browser):
+    browser.get('http://192.168.31.208:8081/admin/')
+    password_form = browser.find_element(By.CSS_SELECTOR, "#input-password")
+    assert password_form.accessible_name == "Password"
+
+
+def test_button_login(browser):
+    browser.get('http://192.168.31.208:8081/admin/')
+    button = browser.find_element(By.CSS_SELECTOR, "button")
+    assert button.aria_role == "button"
+
+
+def test_header(browser):
+    browser.get('http://192.168.31.208:8081/admin/')
+    header = browser.find_element(By.CSS_SELECTOR, 'h1.panel-title')
+    assert header.text == "Please enter your login details."
+
+
+def test_link_forgot(browser):
+    browser.get('http://192.168.31.208:8081/admin/')
+    link_forgot = browser.find_element(By.XPATH, '//span[@class="help-block"]/*')
+    assert link_forgot.get_attribute('href') == "http://192.168.31.208:8081/admin/index.php?route=common/forgotten"
