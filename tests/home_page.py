7d7461b1cd4ff0e16402f4377c841d427d8edc81
Yura <gyrra777@gmail.com>
2023-06-20 16:27:18 +0300

    каталог готов

--- tests/home_page.py
+++ tests/home_page.py
@@ -10,18 +10,23 @@ def test_click_logo_home_page(browser):
     assert old == "http://192.168.31.208:8081/"
     assert new == old + "index.php?route=common/home"
 
+
 def test_featured_space(browser):
     featured = len(browser.find_elements(By.CSS_SELECTOR, ".image"))
     assert 4 == featured
 
 
 def test_carousel_swiper_viewport_bottom(browser):
-    carousel = len(browser.find_elements(By.XPATH, '//div[@class="swiper-pagination carousel0 swiper-pagination-clickable swiper-pagination-bullets"]/*'))
+    carousel = len(browser.find_elements(By.XPATH,
+                                         '//div[@class="swiper-pagination carousel0 swiper-pagination-clickable swiper-pagination-bullets"]/*'))
     assert 11 == carousel
 
+
 def test_carousel_swiper_viewport_top(browser):
-    carousel = len(browser.find_elements(By.XPATH, '//div[@class="swiper-pagination slideshow0 swiper-pagination-clickable swiper-pagination-bullets"]/*'))
+    carousel = len(browser.find_elements(By.XPATH,
+                                         '//div[@class="swiper-pagination slideshow0 swiper-pagination-clickable swiper-pagination-bullets"]/*'))
     assert 2 == carousel
+
 
 def test_featured_text(browser):
     text = "Featured"
