3e8c33c5153526d53b3141ff6ce89f5d0b650a53
Yura <gyrra777@gmail.com>
2023-07-12 19:37:54 +0300

    вариант со слушателем

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
+import os
+import logging
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import FirefoxOptions, ChromeOptions, EdgeOptions
+from selenium import webdriver
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+
+DRIVERS = os.path.expanduser
 
 
 def pytest_addoption(parser):
@@ -9,6 +15,12 @@ def pytest_addoption(parser):
     parser.addoption("--url", default="http://192.168.31.208:8081/")
     parser.addoption("--maximize", action="store_true")
     parser.addoption("--headless", action="store_true")
+    parser.addoption("--log_level", action="store", default="DEBUG")
+
+
+log_map = {
+    "DEBUG": logging.DEBUG
+}
 
 
 @pytest.fixture
@@ -17,6 +29,55 @@ def browser(request):
     maximize = request.config.getoption("--maximize")
     headless = request.config.getoption("--maximize")
     url = request.config.getoption("--url")
+    log_level = request.config.getoption("--log_level")
+
+    class WebdriverListener(AbstractEventListener):
+        logger = logging.getLogger(request.node.name)
+        logger.setLevel(logging.INFO)
+        ch = logging.FileHandler(filename=f"./logs/{request.node.name}.log")
+        ch.setFormatter(logging.Formatter('%(name)s:%(levelname)s %(message)s'))
+        ch.setLevel(log_map[log_level])
+        logger.addHandler(ch)
+
+        def before_navigate_to(self, url, driver):
+            self.logger.info(f"I'm navigating to {url} and {driver.title}")
+
+        def after_navigate_to(self, url, driver):
+            self.logger.info(f"I'm on {url}")
+
+        def before_navigate_back(self, driver):
+            self.logger.info(f"I'm navigating back")
+
+        def after_navigate_back(self, driver):
+            self.logger.info(f"I'm back!")
+
+        def before_find(self, by, value, driver):
+            self.logger.info(f"I'm looking for '{value}' with '{by}'")
+
+        def after_find(self, by, value, driver):
+            self.logger.info(f"I've found '{value}' with '{by}'")
+
+        def before_click(self, element, driver):
+            self.logger.info(f"I'm clicking {element}")
+
+        def after_click(self, element, driver):
+            self.logger.info(f"I've clicked {element}")
+
+        def before_execute_script(self, script, driver):
+            self.logger.info(f"I'm executing '{script}'")
+
+        def after_execute_script(self, script, driver):
+            self.logger.info(f"I've executed '{script}'")
+
+        def before_quit(self, driver):
+            self.logger.info(f"I'm getting ready to terminate {driver}")
+
+        def after_quit(self, driver):
+            self.logger.info(f"Driver Quit")
+
+        # def on_exception(self, exception, driver):
+        #     logger.error(f'Oooops i got: {exception}')
+        #     driver.save_screenshot(f'logs/{driver.session_id}.png')
 
     if browser_name == "firefox":
         options = FirefoxOptions()
@@ -39,6 +100,9 @@ def browser(request):
         driver.get(url)
     else:
         raise ValueError(f"Driver {browser_name} not supported.")
+    driver = EventFiringWebDriver(driver, WebdriverListener())
+    driver.test_name = request.node.name
+    driver.log_level = log_level
 
     if maximize:
         driver.maximize_window()
